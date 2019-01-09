14fe766cda46fe267ec6f821023c4eb90bf5b195
Alexander Fountain <fountain@Alexanders-Air.PK5001Z>
2019-01-09 09:32:38 -0600

    page transitions

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Mobilemenu from '../components/Mobilemenu'
+import PageTransition from 'gatsby-plugin-page-transitions';
 import Navbar from '../components/Navbar'
 import Topsocial from '../components/Topsocial'
 import Footer from '../components/Footer'
@@ -12,11 +12,13 @@ const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Home | Prescriptive" />
     <header id="header" className="header">
-    
+
     <Topsocial />
     <Navbar />
     </header>
+    <PageTransition>
     <div>{children}</div>
+    </PageTransition>
     <Footer />
   </div>
 )
