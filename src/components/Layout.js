a4ee2c87b1ccb95c5d90f4b3991643cbff40972f
Alexander Fountain <fountain@Alexanders-Air.PK5001Z>
2019-01-11 09:42:40 -0600

    page transition

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import posed from 'react-pose'
 import Navbar from '../components/Navbar'
 import Innermenu from '../components/Innermenu'
 import Topsocial from '../components/Topsocial'
 import Footer from '../components/Footer'
 import './all.sass'
 import '../css/main.css'
-
-const Child = posed.p({
-  enter: { opacity: 1 },
-  exit: { opacity: 0 },
-})
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -22,7 +16,7 @@ const TemplateWrapper = ({ children }) => (
     <Navbar />
     <Innermenu />
     </header>
-    <Child>{children}</Child>
+    <div>{children}</div>
     <Footer />
   </div>
 )
